9373126845ce64a4e6484b4275aa3475bf789d6e
Cian Glenfield <cianglenfield@Cians-Mac-mini.local>
2021-04-14 16:43:38 +0900

    Update file

---
+++ japan_vaccine_data.py
@@ -0,0 +1,121 @@
+from __future__ import division
+import scipy
+from scipy import stats
+import numpy
+import os, time
+import sys
+from os import path
+
+from urllib.request import urlopen
+from datetime import date, timedelta
+from datetime import datetime
+# import datetime
+import calendar
+import wget
+
+
+japanese_population = 126300000
+
+
+medical_workers_data_url = 'https://www.kantei.go.jp/jp/content/IRYO-vaccination_data.xlsx'
+elderly_data_url = 'https://www.kantei.go.jp/jp/content/KOREI-vaccination_data.xlsx'
+
+if os.path.exists('/Users/cianglenfield/Desktop/japan_vaccine_data/medical_workers_data_excel.xlsx') == True:
+	os.system('rm /Users/cianglenfield/Desktop/japan_vaccine_data/medical_workers_data_excel.xlsx')
+	os.system('rm /Users/cianglenfield/Desktop/japan_vaccine_data/medical_workers_data_converted.csv')
+
+if os.path.exists('/Users/cianglenfield/Desktop/japan_vaccine_data/elderly_data_excel.xlsx') == True:
+	os.system('rm /Users/cianglenfield/Desktop/japan_vaccine_data/elderly_data_excel.xlsx')
+	os.system('rm /Users/cianglenfield/Desktop/japan_vaccine_data/elderly_data_converted.csv')
+
+wget.download(medical_workers_data_url, '/Users/cianglenfield/Desktop/japan_vaccine_data/medical_workers_data_excel.xlsx')
+os.system('ssconvert medical_workers_data_excel.xlsx medical_workers_data_converted.csv')
+
+wget.download(elderly_data_url, '/Users/cianglenfield/Desktop/japan_vaccine_data/elderly_data_excel.xlsx')
+os.system('ssconvert elderly_data_excel.xlsx elderly_data_converted.csv')
+
+print("")
+
+medical_workers_file = open("medical_workers_data_converted.csv")
+elderly_file = open("elderly_data_converted.csv")
+MHLW_data_file = open("japan_vaccine_MHLW_data.txt")
+
+MHLW_data_file.readline()
+
+coordinated_vaccine_data = {}
+
+for line in medical_workers_file:
+	if "2021" in line:
+		data_date, day, daily_total_medical, daily_first_shot_medical, daily_second_shot_medical = line.rstrip().split(",")
+		date_time = datetime.strptime(data_date, '%Y/%m/%d')
+		new_date = datetime.strftime(date_time, '%a, %b %d')
+		coordinated_vaccine_data[new_date] = [int(daily_total_medical), int(daily_first_shot_medical), int(daily_second_shot_medical)]
+
+for line in elderly_file:
+	if "2021" in line:
+		data_date, day, daily_total_elderly, daily_first_shot_elderly, daily_second_shot_elderly = line.rstrip().split(",")
+		date_time = datetime.strptime(data_date, '%Y/%m/%d')
+		new_date = datetime.strftime(date_time, '%a, %b %d')
+		daily_total_comma = "{:,}".format(int(daily_total_elderly)+coordinated_vaccine_data[new_date][0])
+		coordinated_vaccine_data[new_date] = [int(daily_total_elderly)+coordinated_vaccine_data[new_date][0], int(daily_first_shot_elderly)+coordinated_vaccine_data[new_date][1], int(daily_second_shot_elderly)+coordinated_vaccine_data[new_date][2], daily_total_comma]
+
+
+for line in MHLW_data_file:
+	new_date, daily_total, daily_first_shots, daily_second_shots, daily_total_comma = line.rstrip().split("\t")
+	coordinated_vaccine_data[new_date] = [int(daily_total), int(daily_first_shots), int(daily_second_shots), daily_total_comma]
+
+
+sdate = date(2021, 2, 17)
+edate = datetime.date(datetime.now())
+
+delta = edate - sdate
+
+date_list = []
+
+for i in range(delta.days):
+	day = sdate + timedelta(days=i)
+	new_format = datetime.strftime(day, '%a, %b %d')
+	date_list += [new_format]
+
+for day in date_list:
+	if day not in coordinated_vaccine_data:
+		coordinated_vaccine_data[day] = [0, 0, 0, " "]
+
+graph_data_file = open("japan_vaccine_data_for_graph.txt", "w")
+vaccine_data_summaries = open("/Volumes/Macintosh HD/Users/cianglenfield/Desktop/japan_vaccine_data/japan_vaccine_data_summaries.txt", "w")
+
+graph_data_file.write("Date\tTotal daily doses given\tFirst doses given\tFollow-up doses given\tBasic doses given total\n")
+
+
+overall_total = 0
+first_dose_total = 0
+second_dose_total = 0
+
+
+for day in date_list:
+	graph_data_file.write("%s\t%s\t%s\t%s\t%s\n" % (day, coordinated_vaccine_data[day][0], coordinated_vaccine_data[day][1], coordinated_vaccine_data[day][2], coordinated_vaccine_data[day][3]))
+	overall_total += coordinated_vaccine_data[day][0]
+	first_dose_total += coordinated_vaccine_data[day][1]
+	second_dose_total += coordinated_vaccine_data[day][2]
+
+
+overall_population_coverage = round((overall_total/(japanese_population*2))*100, 2)
+first_shot_coverage = round((first_dose_total/japanese_population)*100, 2)
+second_shot_coverage = round((second_dose_total/japanese_population)*100, 2)
+
+total_coverage_comma = "{:,}".format(overall_total)
+
+vaccine_data_summaries.write("%s\t%s\t%s\t%s\n" % (total_coverage_comma, str(overall_population_coverage), str(first_shot_coverage), str(second_shot_coverage)))
+
+
+elderly_file.close()
+medical_workers_file.close()
+MHLW_data_file.close()
+graph_data_file.close()
+vaccine_data_summaries.close()
+
+os.system("Rscript japan_vaccine_graphs.R")
+
+os.system("cp japan_vaccine_data.py japan_vaccine_graphs.R japan_vaccination_rate_plot.pdf japan_vaccine_data_for_graph.txt japan_vaccine_data_summaries.txt japan-covid19-vaccination-data/")
+
+os.system("cd japan-covid19-vaccination-data/; git add . ; git commit -m \"Update file\"; git push origin main")
---
+++ japan_vaccine_graphs.R
@@ -0,0 +1,31 @@
+suppressWarnings(library(ggplot2))
+suppressWarnings(library(scales))
+suppressWarnings(library(reshape2))
+suppressWarnings(suppressMessages(library(zoo)))
+
+vaccine_data <- read.table("/Volumes/Macintosh HD/Users/cianglenfield/Desktop/japan_vaccine_data/japan_vaccine_data_for_graph.txt", sep = "\t", header = TRUE)
+vaccine_data_summary <- read.table("/Volumes/Macintosh HD/Users/cianglenfield/Desktop/japan_vaccine_data/japan_vaccine_data_summaries.txt", sep = "\t", header = FALSE)
+
+total_shots_given <- vaccine_data_summary$V1[1]
+overall_population_coverage <- vaccine_data_summary$V2[1]
+first_shot_population_coverage <- vaccine_data_summary$V3[1]
+second_shot_population_coverage <- vaccine_data_summary$V4[1]
+
+vaccine_data$Date <- factor(vaccine_data$Date, levels = vaccine_data$Date)
+
+reshaped <- reshape2::melt(vaccine_data, id.vars = c("Date", "Total.daily.doses.given", "Basic.doses.given.total"))
+reshaped$rolledmean = rollapply(vaccine_data$Total.daily.doses.given, width = 7, FUN = mean, align = "center", partial = TRUE)
+
+vaccine_graph <- ggplot(reshaped, aes(x = Date, y = value, fill = variable)) + 
+  geom_col() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.9), plot.title = element_text(face = "bold", hjust = 0.5)) + 
+  scale_y_continuous(name = "Total daily doses given", label = comma, limits = c(0, 200000)) + 
+  geom_text(data = reshaped, aes(x = Date, y = Total.daily.doses.given, label = Basic.doses.given.total, angle = 90, hjust = -0.7)) +
+  geom_line(aes(y = reshaped$rolledmean, group = 1, colour = "7 day rolling\n average")) + 
+  labs(title = "COVID-19 vaccination progress in Japan", caption = "Data sources: Japan Ministry of Health, Labour and Welfare\n and Prime Minister's Office of Japan\nJapan population: 126,300,000 (World Bank)\nVaccines approved (1): Pfizer/BioNTech\nWeekend data is compiled the following Monday") + 
+  scale_fill_manual(name = "", labels = c("First dose", "Second dose"), values = c("#F8766D", "#00BA38")) + 
+  scale_color_manual(name = "", values = c("black")) + 
+  annotate("text", x = 2, y = 190000, label = paste("Total doses given: ", total_shots_given, "\nFirst dose coverage: ", first_shot_population_coverage, "%\nSecond dose coverage: ", second_shot_population_coverage, "%", sep = ""), hjust = 0, vjust = 1)
+ggsave("japan_vaccination_rate_plot.png", plot = vaccine_graph, width = 12, height = 5)
+ggsave("japan_vaccination_rate_plot.pdf", plot = vaccine_graph, width = 12, height = 5)
+
